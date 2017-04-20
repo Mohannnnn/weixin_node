fb7afd6281f7576a64264cd767e2c09eda5a0ec9
Mo Han <1063022109@qq.com>
2017-04-20 11:25:17 +0800

    配置路由

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var wechat = require('./routes/wechat');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 * */
 app.use('/', routes);
 app.use('/users', users);
+app.use('/wechat' , wechat);
 
 /*
 * 加载错误处理方法
--- config.js
+++ config.js
@@ -1,3 +1,11 @@
 /**
  * Created by 14-7447 on 2017/4/18.
  */
+
+const config = {
+    token : 'weixin_node',
+    appid : 'wxc08d0b0fe411dd43',
+    encodingAESkey : 'dtg3u7B5gbHPCttZMm5Uame3JlosOCYABwKjlIZ3EZc'
+};
+
+module.exports = config;
