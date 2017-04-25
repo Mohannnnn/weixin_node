2fec8bdc245d9b4e5ad13d22f6273ed2c993cbaa
Mo Han <1063022109@qq.com>
2017-04-25 21:51:46 +0800

    添加联系

--- menu.js
+++ menu.js
@@ -2,40 +2,51 @@
  * Created by 14-7447 on 2017/4/24.
  */
 
-const menu = {
+const menu =
+{
             "button": [
-            {
-                "name": "关于我",
-                "sub_button": [
-                    {
-                        "type": "view",
-                        "name": "博客",
-                        "url": "https://fututer.github.io/"
-                    },
-                    {
-                        "type": "view",
-                        "name": "Github",
-                        "url": "https://github.com/fututer/"
-                    }
-                ]
-            },
-            {
-                "name": "联系我",
-                "sub_button": [
-                    {
-                        "type": "click",
-                        "name": "QQ",
-                        "key": "V1001_CONNECT_QQ"
-                    },
-                    {
-                        "type": "click",
-                        "name": "微信",
-                        "key": "V1002_CONNECT_WEIXIN"
-                    }
+                {
+                    "name" : "我的位置",
+                    "sub_button" : [
+                        {
+                            "type" : "click",
+                            "name" : "我的位置",
+                            "key"  : "V1002_LOCATION_NOW"
+                        }
+                    ]
+                },
+                {
+                    "name": "关于我",
+                    "sub_button": [
+                        {
+                            "type": "view",
+                            "name": "博客",
+                            "url": "https://fututer.github.io/"
+                        },
+                        {
+                            "type": "view",
+                            "name": "Github",
+                            "url": "https://github.com/fututer/"
+                        }
+                    ]
+                },
+                {
+                    "name": "联系我",
+                    "sub_button": [
+                        {
+                            "type": "click",
+                            "name": "QQ",
+                            "key": "V1001_CONNECT_QQ"
+                        },
+                        {
+                            "type": "click",
+                            "name": "微信",
+                            "key": "V1001_CONNECT_WEIXIN"
+                        }
 
-                ]
-            }
-        ]
+                    ]
+                }
+            ]
 };
 
 module.exports = menu;
--- routes/wechat.js
+++ routes/wechat.js
@@ -9,26 +9,48 @@ const wechat = require('wechat');
 const config = require('../config.js');
 
 
+const location  = {
+    label : '',
+    location_X : '',
+    location_Y : ''
+};
 
 router.use('/' ,  wechat(config ,
     wechat.text(function (message , req , res , next ) {
         console.log(message);
 
-        if (message.Content == 'blog') {
-            res.reply([{
-                title: '我的Blog',
-                description: '我的一些关于前端方面的博客',
+        if (message.Content == 'github') {
+            res.reply([
+                {
+                    title: '我的Github',
+                    decription : '这是我的Github,欢迎start、fork',
+                    picurl: 'http://img2.niushe.com/upload/201304/19/14-22-31-71-26144.jpg',
+                    url: 'https://github.com/fututer'
+                }
+            ]);
+        } else if (message.Content == 'blog') {
+            res.reply(
+            [{
+                title: '我的一些关于前端方面的博客',
                 picurl: 'http://s6.sinaimg.cn/mw690/001whaJHzy6NkfRttDD15&690',
-                url: 'https://fututer.github.io/'
-            }]);
-        } else if (message.Content == 'github') {
-            res.reply([{
-                title: '我的Github',
-                description: '进入我的Github',
+                url: 'https://fututer.github.io/',
+                articleCount : 3
+            },{
+
+                title: 'JavaScript的小技巧',
                 picurl: 'http://img2.niushe.com/upload/201304/19/14-22-31-71-26144.jpg',
-                url: 'https://github.com/fututer'
-            }]);
-        } else if ( typeof(parseInt(message.Content)) == 'number' && message.Content != '') {
+                url: 'https://fututer.github.io/2017/02/23/JavaScript%E7%9A%84%E4%B8%80%E4%BA%9B%E5%B0%8F%E6%8A%80%E5%B7%A7/'
+            }, {
+                title: 'js Promise异步执行',
+                picurl: 'http://img2.niushe.com/upload/201304/19/14-22-31-71-26144.jpg',
+                url: 'https://fututer.github.io/2016/11/24/js-Promise%E5%BC%82%E6%AD%A5%E6%89%A7%E8%A1%8C/'
+            }, {
+                title : 'js原型及原型链',
+                picurl : 'http://img2.niushe.com/upload/201304/19/14-22-31-71-26144.jpg',
+                url : 'https://fututer.github.io/2016/11/23/js-%E5%8E%9F%E5%9E%8B%E5%8F%8A%E5%8E%9F%E5%9E%8B%E9%93%BE/'
+            }
+            ]);
+        } else if ( typeof(parseInt(message.Content)) == 'number' && /^[0-9]*$/.test(message.Content)) {
             const data = parseInt(message.Content);
             const dataArr = [];
 
@@ -48,10 +70,10 @@ router.use('/' ,  wechat(config ,
             res.reply({
                 type : 'text',
                 content : message.Content + "\n\n" +
-                        '回复 blog 查看我的博客' + "\n" +
-                        '回复 github 访问我的github' + "\n" +
+                        '回复blog查看我的博客' + "\n" +
+                        '回复github访问我的github' + "\n" +
                         '回复任意非负整数字，返回其所有约数' + "\n" +
-                        '回复其他, 返回你发的数据'
+                        '回复某些关键字（包括语音）, 将会有彩蛋哦(⊙﹏⊙)'
             });
         }
     }).image(function (message ,req ,res ,next) {
@@ -65,10 +87,58 @@ router.use('/' ,  wechat(config ,
         );
     }).voice(function (message , req ,res ,next) {
         console.log(message);
-        res.reply({
-            type  : 'text',
-            content : message.Recognition
-        });
+        const recognition = message.Recognition;
+
+        if(recognition.indexOf('github') != -1 || recognition.indexOf('不') != -1) {
+            res.reply([
+                {
+                    title: '这是我的Github,欢迎start、fork',
+                    decription : '您说的是：' + recognition + '\n\n' + '是否想找：github',
+                    picurl: 'http://img2.niushe.com/upload/201304/19/14-22-31-71-26144.jpg',
+                    url: 'https://github.com/fututer'
+                }
+            ]);
+        } else if (recognition.indexOf('blog') != -1 || recognition.indexOf('博') != -1 || recognition.indexOf('前端') != -1 || recognition.indexOf('客') != -1) {
+            res.reply(
+                [{
+                    title: '我的一些关于前端方面的博客',
+                    picurl: 'http://s6.sinaimg.cn/mw690/001whaJHzy6NkfRttDD15&690',
+                    url: 'https://fututer.github.io/',
+                    articleCount : 3
+                },{
+
+                    title: 'JavaScript的小技巧',
+                    picurl: 'http://img2.niushe.com/upload/201304/19/14-22-31-71-26144.jpg',
+                    url: 'https://fututer.github.io/2017/02/23/JavaScript%E7%9A%84%E4%B8%80%E4%BA%9B%E5%B0%8F%E6%8A%80%E5%B7%A7/'
+                }, {
+                    title: 'js Promise异步执行',
+                    picurl: 'http://img2.niushe.com/upload/201304/19/14-22-31-71-26144.jpg',
+                    url: 'https://fututer.github.io/2016/11/24/js-Promise%E5%BC%82%E6%AD%A5%E6%89%A7%E8%A1%8C/'
+                }, {
+                    title : 'js原型及原型链',
+                    picurl : 'http://img2.niushe.com/upload/201304/19/14-22-31-71-26144.jpg',
+                    url : 'https://fututer.github.io/2016/11/23/js-%E5%8E%9F%E5%9E%8B%E5%8F%8A%E5%8E%9F%E5%9E%8B%E9%93%BE/'
+                }
+                ]);
+        } else if ( recognition.indexOf('QQ') != -1) {
+            res.reply([{
+                title : '扫描二维码加我QQ',
+                picurl: 'http://user.qzone.qq.com/1063022109/311/',
+                url : 'http://user.qzone.qq.com/1063022109/311/'
+            }]);
+        } else if (recognition.indexOf('微信') != -1 || recognition.indexOf('信') != -1 ) {
+            res.reply([{
+                title : '扫描二维码加我微信',
+                picurl: 'http://a3.qpic.cn/psb?/V121vZvA3qNkcO/jyXxR3xaQI6YvWfgN3GpEJ0DqeyFgJpZwDbF14jxhFg!/m/dGoBAAAAAAAA&ek=1&kp=1&pt=0&bo=rgGuAQAAAAARECc!&tm=1493121600&sce=60-3-3&rf=0-0',
+                url : 'http://a3.qpic.cn/psb?/V121vZvA3qNkcO/jyXxR3xaQI6YvWfgN3GpEJ0DqeyFgJpZwDbF14jxhFg!/m/dGoBAAAAAAAA&ek=1&kp=1&pt=0&bo=rgGuAQAAAAARECc!&tm=1493121600&sce=60-3-3&rf=0-0'
+            }]);
+        } else {
+            res.reply({
+                type  : 'text',
+                content : '您说：' + message.Recognition
+            });
+        }
+
     }).video(function (message , req ,res , next) {
         console.log(message);
         res.reply({
@@ -97,29 +167,61 @@ router.use('/' ,  wechat(config ,
         switch (message.Event) {
             case 'subscribe':
                 var openid=message.FromUserName;
-
                 res.reply({
                     type : 'text',
                     content:'欢迎关注Ring纪念坊~~~' +"\n\n" +
                             '回复 blog 查看我的博客' + "\n" +
                             '回复 github 访问我的github' + "\n" +
                             '回复任意非负整数字，返回其所有公约数' + "\n" +
-                            '回复其他, 返回你发的数据'
+                            '回复某些关键字（包括语音）, 将会有彩蛋哦(⊙﹏⊙)'
                 });
                 break;
             case 'unsubscribe':
-                var openid=message.FromUserName;
-
                 res.reply('亲,请不要离开我!!');
                 break;
-            case 'click' :
-                res.reply('您点击了菜单：'+ message.EventKey);
+            case 'LOCATION' :
+                location.label = message.Label;
+                location.location_X = Math.round(message.Location_X*100)/100;
+                location.location_Y = Math.round(message.Location_Y*100)/100
+                break;
+            case 'CLICK' :
+                switch (message.EventKey) {
+                    case 'V1001_CONNECT_QQ' :
+                        res.reply([{
+                            title : '扫描二维码加我QQ',
+                            picurl: 'http://user.qzone.qq.com/1063022109/311/',
+                            url : 'http://user.qzone.qq.com/1063022109/311/'
+                        }]);
+                        break;
+                    case 'V1001_CONNECT_weixin' :
+                        res.reply([{
+                            title : '扫描二维码加我微信',
+                            picurl: 'http://a3.qpic.cn/psb?/V121vZvA3qNkcO/jyXxR3xaQI6YvWfgN3GpEJ0DqeyFgJpZwDbF14jxhFg!/m/dGoBAAAAAAAA&ek=1&kp=1&pt=0&bo=rgGuAQAAAAARECc!&tm=1493121600&sce=60-3-3&rf=0-0',
+                            url : 'http://a3.qpic.cn/psb?/V121vZvA3qNkcO/jyXxR3xaQI6YvWfgN3GpEJ0DqeyFgJpZwDbF14jxhFg!/m/dGoBAAAAAAAA&ek=1&kp=1&pt=0&bo=rgGuAQAAAAARECc!&tm=1493121600&sce=60-3-3&rf=0-0'
+                        }]);
+                        break;
+                    case 'V1002_LOCATION_NOW' :
+                        res.reply({
+                            type : 'text',
+                            content: '您的地理位置是：' +  location.label + "\n" +
+                            '纬度：' + location.location_X + '\n' +
+                            '经度：' + location.location_Y
+                        });
+                        break;
+                }
                 break;
             case 'SCAN' :
                 res.reply('关注后扫描二维码：'+ message.Ticket);
                 break;
             default :
-                res.send('');
+                res.reply({
+                    type : 'text',
+                    content:'欢迎关注Ring纪念坊~~~' +"\n\n" +
+                    '回复blog查看我的博客' + "\n" +
+                    '回复github访问我的github' + "\n" +
+                    '回复任意非负整数字，返回其所有公约数' + "\n" +
+                    '回复某些关键字（包括语音）, 将会有彩蛋哦(⊙﹏⊙)'
+                });
         }
 
     }).device_text(function (message ,req , res ,next) {
